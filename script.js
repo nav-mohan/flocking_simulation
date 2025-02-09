cc4e4a98e79198a579dd625477be312884e0c045
nav-mohan <navaneeth.mohan94@gmail.com>
2025-02-09 16:45:58 -0500

    implement updateBoids

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+function updateBoids() 
+{
+    for (let boid of boids) 
+    {
+        let avgX = 0, avgY = 0, avgVX = 0, avgVY = 0, countNeighbors = 0;
+        let sepX = 0, sepY = 0;
+        
+        for (let other of boids) 
+        {
+            if(other == boid) continue;
+
+            let dx = other.x - boid.x;
+            let dy = other.y - boid.y;
+            let dist = Math.sqrt(dx * dx + dy * dy);
+            
+            if (dist < neighborDist) {
+                avgX += other.x;
+                avgY += other.y;
+                avgVX += other.vx;
+                avgVY += other.vy;
+                countNeighbors++;
+            }
+            
+            if (dist < separationDist && dist > 0) {
+                sepX -= dx / dist;
+                sepY -= dy / dist;
+            }
+        }
+        
+        if (countNeighbors > 0) 
+        {
+            avgX /= countNeighbors;
+            avgY /= countNeighbors;
+            avgVX /= countNeighbors;
+            avgVY /= countNeighbors;
+            
+            boid.vx += (avgVX - boid.vx) * alignFactor; 
+            boid.vy += (avgVY - boid.vy) * alignFactor;
+            
+            boid.vx += (avgX - boid.x) * cohesionFactor;
+            boid.vy += (avgY - boid.y) * cohesionFactor;
+        }
+        
+        boid.vx += sepX * separationFactor;
+        boid.vy += sepY * separationFactor;
+        
+        let speed = Math.sqrt(boid.vx * boid.vx + boid.vy * boid.vy);
+        if (speed > 4) {
+            boid.vx = (boid.vx / speed) * 4;
+            boid.vy = (boid.vy / speed) * 4;
+        }
+        
+        boid.x += boid.vx;
+        boid.y += boid.vy;
+        
+        if (boid.x < 0) boid.x = canvas.width;
+        if (boid.x > canvas.width) boid.x = 0;
+        if (boid.y < 0) boid.y = canvas.height;
+        if (boid.y > canvas.height) boid.y = 0;
+    }
+}
+
 function drawBoids() 
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -12,7 +74,9 @@ function drawBoids()
 
 function animate() 
 {
+    updateBoids();
     drawBoids();
+    requestAnimationFrame(animate);
 }
 
 
